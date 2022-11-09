3569f9717083265213b97e3022323eddf51ebef0
yanooomm <115118650+yanooomm@users.noreply.github.com>
2022-11-09 19:40:18 +0500

    Add files via upload

---
+++ дз5-1.cpp
@@ -0,0 +1,28 @@
+﻿// Reccur4 Написать реккурсивный алгоритм нахождения максимальной цифры целого числа N
+
+#include <iostream>
+
+using namespace std;
+
+void rec(int a, int &MaxVar)
+{
+	int n = a % 10;
+	if (n > MaxVar)
+		MaxVar = n;
+	if (a > 10)
+		rec(a /= 10, MaxVar);
+}
+
+
+int main()
+{
+	int a;
+	cin >> a;
+	int MaxVar = 0;
+	rec(a, MaxVar);
+	cout << MaxVar;
+}
+
+// 1234321 -> 4
+// 11111 -> 1
+// 0987890 -> 9
